4789cd58ba48dfb5403de656a726aac5b1378573
Rubeneg27 <115479882+Rubeneg27@users.noreply.github.com>
2023-01-26 14:43:04 +0000

    Continuamos la página Little Lemon

--- excercises/src/Componentes/Header.js
+++ excercises/src/Componentes/Header.js
@@ -1,17 +1,23 @@
 import React from "react"
 
+const logo = {
+  url:"",
+  text: "Little Lemon"
+}
+
 
 function Header () {
  return (
     <header>          
 			<div className="logo-container">
-        <img src="" alt="logo"/>
+        <img src="logo.url" alt="logo"/>
+        <h2>{logo.text}</h2>
       </div>
 			<ul className="nav-container">
         <li><a href="#home">Home</a></li>
         <li><a href="#About">About</a></li>
         <li><a href="#About">Menu</a></li>
-        <li><a href="#About">Reservations</a></li>
+        <li><a href="#About">Book</a></li>
         <li><a href="#About">Order online</a></li>        
 			  <li><a href="#About">Login</a></li>
       </ul>
